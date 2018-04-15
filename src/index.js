4392a350aca782093a8fedbd18f88974eb662040
hubLuo <hubluo@126.com>
2018-04-15 21:49:39 +0800

    引入路由

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import{Router,Route,hashHistory,IndexRedirect} from "react-router";
+import BasicForm from "./components/forms/BasicForm"
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Router history={hashHistory}>
+        <Route path={"app"} component={App}></Route>
+        <Route path={"form"} component={BasicForm}></Route>
+        {/* <App /> */}
+    </Router>,
+    document.getElementById('root')
 );
 
