4b8c95cfde0ac4bfa448a0da17e5d8070e294436
hubLuo <hubluo@126.com>
2018-04-15 21:25:33 +0800

    add sider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, {Component} from "react";
 import { Layout } from "antd";
 import Header from "./components/headerCustom";
+import Sider from "./components/SiderCustom";
 import "./style/index.less";
+
+const {Content,Footer}=Layout;
+
 class App extends Component{
     render(){
         return(
             <Layout>
+                <Sider />
                 <Layout>
-                    <h1>ddf</h1>
                     <Header />
+                    <Content style={{ margin: '0 16px',overflow:"initial" }}>
+                     Hello
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2016 Created by Ant UED
+                    </Footer>
                 </Layout>
             </Layout>
         );
