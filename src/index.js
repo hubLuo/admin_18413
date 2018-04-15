20056481d0acca032962566d0f6d76467ef0ba84
hubLuo <hubluo@126.com>
2018-04-15 22:45:06 +0800

    添加默认路由跳转功能：子组件渲染原理封装为page文件，当需要动态变化内容时，可以调用。也就说，需要内容动态变化时，只需要子组件可渲染，就能实现动态变化。

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import{Router,Route,hashHistory,IndexRedirect} from "react-router";
-import BasicForm from "./components/forms/BasicForm"
+import BasicForm from "./components/forms/BasicForm";
+import Page from "./components/page";
 ReactDOM.render(
     <Router history={hashHistory}>
-        <Route path={"app"} component={App}>
-            <Route path={"form"} component={BasicForm}></Route>
+        <Route path={"/"} component={Page}>
+            {/*该方式为react3*/}
+            <IndexRedirect  to="/app/form"/>
+            <Route path={"app"} component={App}>
+                <Route path={"form"} component={BasicForm}></Route>
+            </Route>
         </Route>
         {/* <App /> */}
     </Router>,
